bc73b071b39e82549a3e5f6696a151d3562226c6
Juan Amiguet Vercher <jamiguet@gmail.com>
2017-10-21 22:57:17 +0200

    Adds slack and morris

--- apt_install.sh
+++ apt_install.sh
@@ -19,10 +19,12 @@ sudo apt-key adv --keyserver keyserver.ubuntu.com --recv-keys 573E3D1C51AE1B3D
 sudo apt update
 
 
-cat <<EOF > packages.list
-emacs24 vim spideroakone subversion octave spotify-client scala
-vim latexmk texlive-fonts-recommended texlive-latex-extra transfig
-epstool fort77 libpcre3 libpcre3-dev 
+cat <<EOF > packages.list 
+
+emacs24 vim spideroakone subversion octave spotify-client scala vim
+latexmk texlive-fonts-recommended texlive-latex-extra transfig epstool
+fort77 libpcre3 libpcre3-dev slack-desktop morris
+
 EOF
 
 

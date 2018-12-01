204e7e5a8594deace613eb7cd3839079a042d7a6
Juan Amiguet Vercher <jamiguet@gmail.com>
2018-12-01 08:17:47 +0100

    fixes python and hy-lang install errors

--- python.sh
+++ python.sh
@@ -4,13 +4,13 @@
 
 sudo apt install python-pip python3-distutils
 
-pip install virtualenvwrapper
+sudo -H pip install virtualenvwrapper
 
 mkdir -p /home/jamiguet/.virtualenvs
 virtualenv -p /usr/bin/python3 /home/jamiguet/.virtualenvs/hy-land
 
 source /home/jamiguet/.virtualenvs/hy-land/bin/activate
-pip install git+https://github.com/hylang/hy.git
-pip install git+https://github.com/hylang/hy.git
+sudo -H pip install git+https://github.com/hylang/hy.git
+sudo -H pip install git+https://github.com/hylang/hy.git
 
-chown -t jamiguet:jamiguet /home/jamiguet/.virtualenvs
+sudo chown -r jamiguet:jamiguet /home/jamiguet/.virtualenvs
--- spiderOak.sh
+++ spiderOak.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
 
+echo "Logging into SpiderOak" 
 SpiderOakONE --setup=-
 
